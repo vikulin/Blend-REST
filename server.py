acb4778003e9a8acc2d63de18c12d0728b76745f
Vadym Vikulin <vadym.vikulin@rivchain.org>
2025-10-28 11:35:51 +0200

    Arbitrary primitive type in create_object action

--- server.py
+++ server.py
@@ -73,11 +73,24 @@ def process_commands():
         if action == "create_object":
             typ = cmd["type"]
             p = cmd.get("params", {})
+            
+            # Create any primitive type
             if typ == "cube":
                 bpy.ops.mesh.primitive_cube_add(**p)
             elif typ == "cylinder":
                 bpy.ops.mesh.primitive_cylinder_add(**p)
-            # ... other types
+            elif typ == "uv_sphere":
+                bpy.ops.mesh.primitive_uv_sphere_add(**p)
+            elif typ == "ico_sphere":
+                bpy.ops.mesh.primitive_ico_sphere_add(**p)
+            elif typ == "cone":
+                bpy.ops.mesh.primitive_cone_add(**p)
+            elif typ == "torus":
+                bpy.ops.mesh.primitive_torus_add(**p)
+            elif typ == "plane":
+                bpy.ops.mesh.primitive_plane_add(**p)
+            else:
+                print(f"Error: Unsupported primitive type '{typ}'")
             
         elif action == "modify_object":
             obj_name = cmd.get("name")
